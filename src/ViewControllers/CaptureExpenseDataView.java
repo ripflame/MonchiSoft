53b3cb9a20b9ebd83a9d8bb63fe9da9e6f7a76ce
Ileana <Ileana@Ileana-PC>
2012-11-26 12:18:04 -0600

    Agregué el JDateChooser a la vista Expenses

--- src/ViewControllers/CaptureExpenseDataView.java
+++ src/ViewControllers/CaptureExpenseDataView.java
@@ -123,8 +123,8 @@ public class CaptureExpenseDataView extends javax.swing.JDialog {
         DescriptionLabel = new javax.swing.JLabel();
         supplierLabel = new javax.swing.JLabel();
         descriptionField = new javax.swing.JTextField();
-        dateField = new com.toedter.calendar.JDateChooser();
         supplierField = new javax.swing.JTextField();
+        dateField = new com.toedter.calendar.JDateChooser();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -172,17 +172,17 @@ public class CaptureExpenseDataView extends javax.swing.JDialog {
                             .addComponent(DateLabel))
                         .addGap(30, 30, 30)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(dateField, javax.swing.GroupLayout.DEFAULT_SIZE, 193, Short.MAX_VALUE)
-                            .addComponent(totalField)
+                            .addComponent(totalField, javax.swing.GroupLayout.DEFAULT_SIZE, 193, Short.MAX_VALUE)
                             .addComponent(descriptionField)
-                            .addComponent(supplierField))
+                            .addComponent(supplierField)
+                            .addComponent(dateField, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(44, 44, 44))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(114, 114, 114)
                         .addComponent(saveButton)
                         .addGap(31, 31, 31)
                         .addComponent(cancelButton)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(0, 68, Short.MAX_VALUE)))
                 .addGap(41, 41, 41))
         );
         layout.setVerticalGroup(
@@ -200,11 +200,11 @@ public class CaptureExpenseDataView extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(TotalLabel)
                     .addComponent(totalField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(DateLabel)
                     .addComponent(dateField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(saveButton)
                     .addComponent(cancelButton))
