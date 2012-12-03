8cecb6c31ca49776f792bb34934e608fcb08ba78
Ileana <Ileana@Ileana-PC>
2012-12-03 14:32:04 -0600

    ManagementComponents: se añadió firmas de métodos y constantes.

--- src/ViewControllers/ManagementComponents.java
+++ src/ViewControllers/ManagementComponents.java
@@ -16,5 +16,13 @@ public interface ManagementComponents {
     //public abstract void 
     public abstract DefaultTableModel getModelTable ();
     public abstract void displayCaptureView ();
+    public abstract void performRemovalProcedures();
+    public abstract void performModificationProcedures();
+    public abstract void performAddingProcedures();
+    public abstract void performSearchingProcedures();
     
+    public final String NEW_COMMAND = "Nuevo";
+    public final String MODIFY_COMMAND = "Modificar";
+    public final String REMOVE_COMMAND = "Eliminar";
+    public final String SHOW_ALL_COMMAND = "Mostrar todos";
 }
