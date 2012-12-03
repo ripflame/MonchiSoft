0b9cfcf29047032b558b4a22431205a451886ba7
Ileana <Ileana@Ileana-PC>
2012-12-03 00:46:37 -0600

    Añado la clase OtherProductManagementComponents.

---
+++ src/ViewControllers/OtherProductManagementComponents.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ViewControllers;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Ileana
+ */
+public class OtherProductManagementComponents extends ManagementComponents{
+
+    @Override
+    public void displayCaptureView() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+   
+    @Override
+    public DefaultTableModel getModelTable() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
