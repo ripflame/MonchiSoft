ddac1e44be35e28599c101aa791412915294d06e
Ileana <Ileana@Ileana-PC>
2012-12-03 00:46:56 -0600

    Añado la clase ToppingManagementComponents.

---
+++ src/ViewControllers/ToppingManagementComponents.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ViewControllers;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Ileana
+ */
+public class ToppingManagementComponents extends ManagementComponents{
+
+    @Override
+    public void displayCaptureView() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    
+
+    @Override
+    public  DefaultTableModel getModelTable() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
