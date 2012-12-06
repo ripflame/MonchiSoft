991db6676e022769c37162770b7aab22417ef86f
ripflame <ripflame@gmail.com>
2012-12-06 10:42:20 -0600

    vista de corte de caja

--- src/Helpers/MessageDisplayManager.java
+++ src/Helpers/MessageDisplayManager.java
@@ -8,6 +8,7 @@ import javax.swing.JOptionPane;
  * @author Gilberto Leon <ripflame@gmail.com>
  */
 public class MessageDisplayManager {
+
     private static final String NO_CELL_SELECTED_STRING = "No seleccionaste ninguna celda";
     private static final String SEARCH_FIELD_EMPTY_STRING = "No escribiste nada en el campo.";
     private static final String NO_COSTUMER_FOUND_STRING = "No se encontró ningún cliente";
@@ -21,7 +22,8 @@ public class MessageDisplayManager {
     private static final String ERROR_DATA_BASE = "Error al guardar. Intente de nuevo.";
     private static final String INVALID_DATA = "Se requiere datos válidos.";
     private static final String PRODUCT_EXISTS = "El producto ya existe.";
-    
+    private static final String NO_DAILY_CASH_CLOSINGS_FOUND = "No se encontraron cortes de caja";
+
     public static void showInformation(MessageType type, Window parentFrame) {
         String dialogMessage = getMessageStringFromType(type);
         JOptionPane.showConfirmDialog(parentFrame,
@@ -30,7 +32,7 @@ public class MessageDisplayManager {
                 JOptionPane.DEFAULT_OPTION,
                 JOptionPane.INFORMATION_MESSAGE);
     }
-    
+
     public static void showError(MessageType type, Window parentFrame) {
         String dialogMessage = getMessageStringFromType(type);
         JOptionPane.showConfirmDialog(parentFrame,
@@ -39,58 +41,60 @@ public class MessageDisplayManager {
                 JOptionPane.DEFAULT_OPTION,
                 JOptionPane.ERROR_MESSAGE);
     }
-    
+
     private static String getMessageStringFromType(MessageType type) {
         String messageString = "";
-        
-        switch(type) {
+
+        switch (type) {
             case NO_CELL_SELECTED:
                 messageString = NO_CELL_SELECTED_STRING;
                 break;
-                
+
             case SEARCH_FIELD_EMPTY:
                 messageString = SEARCH_FIELD_EMPTY_STRING;
                 break;
-                
+
             case NO_COSTUMER_FOUND:
                 messageString = NO_COSTUMER_FOUND_STRING;
                 break;
-                
+
             case NO_SUPPLIER_FOUND:
                 messageString = NO_SUPPLIER_FOUND_STRING;
                 break;
-            
+
             case EMPTY_FIELDS:
                 messageString = EMPTY_FIELDS_STRING;
                 break;
-                
+
             case NO_EXPENSE_FOUND:
                 messageString = NO_EXPENSE_FOUND;
                 break;
-                
+
             case REQUIRED_TEXT:
                 messageString = REQUIRED_TEXT;
                 break;
-                
+
             case REQUIRED_NUM:
                 messageString = REQUIRED_NUM;
                 break;
-                
+
             case ERROR_DATA_BASE:
                 messageString = ERROR_DATA_BASE;
                 break;
-                
+
             case INVALID_DATA:
                 messageString = INVALID_DATA;
                 break;
-                
+
             case PRODUCT_EXISTS:
                 messageString = PRODUCT_EXISTS;
-                break;    
-                        
-                        
+                break;
+                
+            case NO_DAILY_CASH_CLOSINGS_FOUND:
+                messageString = NO_DAILY_CASH_CLOSINGS_FOUND;
+                break;
         }
-        
+
         return messageString;
     }
 }
